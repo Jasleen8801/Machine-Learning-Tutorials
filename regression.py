d42c5832ce48fccbc6940bce5d8990ee819ec1c7
Jasleen8801 <jkaur1_be21@thapar.edu>
2022-12-29 10:42:55 +0530

    Create linearregression.pickle

--- regression.py
+++ regression.py
@@ -7,6 +7,7 @@ from sklearn import preprocessing, svm, model_selection
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 from matplotlib import style
+import pickle
 
 style.use('ggplot')
 
@@ -37,8 +38,14 @@ y = np.array(df['label'])
 X_train, X_test, y_train, y_test = model_selection.train_test_split(
     X, y, test_size=0.2)
 
-clf = LinearRegression(n_jobs=-1)
-clf.fit(X_train, y_train)
+# clf = LinearRegression(n_jobs=-1)
+# clf.fit(X_train, y_train)
+# with open('linearregression.pickle', 'wb') as f:
+#     pickle.dump(clf, f)
+
+pickle_in = open('linearregression.pickle', 'rb')
+clf = pickle.load(pickle_in)
+
 accuracy = clf.score(X_test, y_test)
 # print(accuracy)
 
@@ -62,4 +69,4 @@ df['Forecast'].plot()
 plt.legend(loc=4)
 plt.xlabel('Date')
 plt.ylabel('Price')
-# plt.show()
+plt.show()
