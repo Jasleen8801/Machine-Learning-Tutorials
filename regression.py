18e09fbd99c9e127168741f8618b6a340c4e5e24
Jasleen8801 <jkaur1_be21@thapar.edu>
2022-12-29 11:35:21 +0530

    update regression.py

--- regression.py
+++ regression.py
@@ -17,17 +17,18 @@ df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj. Close'] * 100.0
 df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / \
     df['Adj. Open'] * 100.0
 
+#          price         x            x             x
 df = df[['Adj. Close', 'HL_PCT', 'PCT_change', 'Adj. Volume']]
 
 forecast_col = 'Adj. Close'
 df.fillna(-99999, inplace=True)
 
-forecast_out = int(math.ceil(0.01*len(df)))
+forecast_out = int(math.ceil(0.1*len(df)))
 # print(forecast_out)
 
 df['label'] = df[forecast_col].shift(-forecast_out)
 
-X = np.array(df.drop(['label'], 1))
+X = np.array(df.drop(['label', 'Adj. Close'], 1))
 X = preprocessing.scale(X)
 X = X[:-forecast_out]
 X_lately = X[-forecast_out:]
