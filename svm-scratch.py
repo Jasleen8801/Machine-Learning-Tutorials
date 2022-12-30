1500c9cc2b3ccc8407d3be9b5dad7cfefc2abb00
Jasleen8801 <jkaur1_be21@thapar.edu>
2022-12-30 12:59:53 +0530

    create svm-scratch.py

---
+++ svm-scratch.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+from matplotlib import style
+import numpy as np
+
+style.use('ggplot')
+
+
+class SupportVectorMachine:
+    def __init__(self, visualization=True) -> None:
+        self.visualization = visualization
+        self.colors = {1: 'r', -1: 'b'}
+        if self.visualization:
+            self.fig = plt.figure()
+            self.ax = self.fig.add_subplot(1, 1, 1)
+
+    def fit(self, data):
+        self.data = data
+        opt_dict = {}
+
+        transforms = [[1, 1], [-1, 1], [-1, -1], [1, -1]]
+
+        all_data = []
+        for yi in self.data:
+            for featureset in self.data[yi]:
+                for feature in featureset:
+                    all_data.append(feature)
+
+        self.max_feature_value = max(all_data)
+        self.min_feature_value = min(all_data)
+        all_data = None
+
+        # support vectors yi(xi.w+b) = 1
+
+        step_sizes = [self.max_feature_value + 0.1,
+                      self.max_feature_value + 0.01,
+                      self.max_feature_value + 0.001, ]  # point of expense
+
+        b_range_multiple = 5  # extremely expensive
+        b_multiple = 5  # we don't need to take as small of steps with b as we do w
+
+        latest_optimum = self.max_feature_value*10
+
+        for step in step_sizes:
+            w = np.array([latest_optimum, latest_optimum])
+            optimized = False  # we can do this because SVM is a connvex optimized problem
+            while not optimized:
+                for b in np.arange(-1*(self.max_feature_value * b_range_multiple),
+                                   self.max_feature_value*b_range_multiple,
+                                   step=step*b_multiple):
+                    for transformation in transforms:
+                        w_t = w * transformation
+                        found_option = True
+                        for i in self.data:  # weakest link in the svm fundamentally
+                            # SMO attempts to fix this a bit
+                            # yi(xi.w+b) >= 1
+                            for xi in self.data[i]:
+                                yi = i
+                                if not yi*(np.dot(w_t, xi)+b) >= 1:
+                                    found_option = False
+                                    break
+                            if found_option:
+                                opt_dict[np.linalg.norm(w_t)] = [w_t, b]
+
+                if w[0] < 0:
+                    optimized = True
+                    print('Optimized a step')
+                else:
+                    w = w - step
+
+            norms = sorted([n for n in opt_dict])  # ||w||: [w,b]
+            opt_choice = opt_choice[norms[0]]
+
+            self.w = opt_choice[0]
+            self.b = opt_choice[1]
+
+            latest_optimum = opt_choice[0][0]+step*2
+
+    def predict(self, features):
+        classification = np.sign(np.dot(np.array(features), self.w)+self.b)
+        if classification != 0 and self.visualization:
+            self.ax.scatter(features[0], features[1], s=200, c=self.colors[classification], marker='*')
+        return classification
+
+    def visualize(self):
+        pass
+
+
+data_dict = {
+    -1: np.array([[1, 7], [2, 8], [3, 8]]),
+    1: np.array([5, 1], [6, -1], [7, 3])
+}
